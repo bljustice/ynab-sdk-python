2a081f0480b6d9fcaf682a0187d1d6e0dc379e49
Blake Justice <13934264+bljustice@users.noreply.github.com>
2022-08-07 20:32:16 -0500

    Rewrite budget client

--- ynab/client.py
+++ ynab/client.py
@@ -9,7 +9,6 @@ class YNABClient:
     def __init__(self, personal_token: str) -> None:
 
         self.personal_token = personal_token
-        
 
     def _make_bearer_header(self) -> dict:
         """
@@ -20,14 +19,11 @@ class YNABClient:
             'Authorization': f'Bearer {self.personal_token}'
         }
 
-
-    def _make_request(self, endpoint: str, **kwargs) -> requests.Response:
+    def _make_request(self, api_endpoint: str, **kwargs) -> requests.Response:
         """
         Build and make a GET request to subsection of YNAB API
         """
-        base_endpoint = 'https://api.youneedabudget.com/v1'
         bearer_header = self._make_bearer_header()
-
-        final_endpoint = f'{base_endpoint}/{endpoint}'
+        final_endpoint = f'https://api.youneedabudget.com/v1/budgets/{api_endpoint}'
         r = requests.get(final_endpoint, params={**kwargs}, headers=bearer_header)
         return r
